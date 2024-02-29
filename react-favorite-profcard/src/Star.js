91caa8ad5137dae6d80d26b05ccd22fe27ce14f2
Olivia Wong <oliviawong@outlook.com>
2024-02-29 13:40:37 +0100

    star icon modified

--- react-favorite-profcard/src/Star.js
+++ react-favorite-profcard/src/Star.js
@@ -2,12 +2,20 @@ import React from "react"
 
 export default function Star(props) {
     const starIcon = props.isFilled ? "star-filled.jpg" : "star-empty.jpg"
+    const buttonLabel = props.isFilled ? "Unmark as favorite" : "Mark as favorite"
     return (
+      <button
+        onClick={props.handleClick}
+        className="star--icon"
+        aria-label={buttonLabel}
+        aria-pressed={props.isFilled}
+      >
         <img
             src={`../images/${starIcon}`}
             className="card--favorite"
-            onClick={props.handleClick}
             alt=".."
         />
+      </button>
+
     )
 }
